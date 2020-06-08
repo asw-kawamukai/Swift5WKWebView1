7e4e86e420f273c6735f505b590bb56ac1317646
川向　亮 <r.kawamukai@a-sw.jp>
2020-06-08 11:09:41 +0900

    Initial Commit

---
+++ Swift5WKWevView1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5WKWevView1
+//
+//  Created by ASW Kawamukai on 2020/06/08.
+//  Copyright © 2020 ASW Kawamukai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
