fb6ce730c8164f95306d4c4068af9185c1979046
川向　亮 <r.kawamukai@a-sw.jp>
2020-06-08 13:05:19 +0900

    First Commit

--- Swift5WKWevView1/ViewController.swift
+++ Swift5WKWevView1/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,WKNavigationDelegate {
+    
+    var webView = WKWebView()
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+   @IBOutlet weak var toolBar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        webView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - toolBar.frame.height*2)
+        
+        view.addSubview(webView)
+        
+        webView.navigationDelegate = self
+        
+        let url = URL(string: "https://www.yahoo.co.jp")
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("読み込み開始")
+        indicator.startAnimating()
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("読み込み完了")
+        
+        indicator.stopAnimating()
     }
 
-
+    @IBAction func goPage(_ sender: Any) {
+        webView.goForward()
+        
+    }
+    
+    @IBAction func backPage(_ sender: Any) {
+        webView.goBack()
+    
+    }
 }
 
